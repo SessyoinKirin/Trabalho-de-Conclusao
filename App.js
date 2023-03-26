0949ac7a5d5e568814a7625646844cafa3b7a192
Haohui Chen <neosdiva@gmail.com>
2023-03-26 20:18:49 -0300

    limitando linha de descrição e deixando os pouco mais visível, mas ainda esta com problema

--- App.js
+++ App.js
@@ -2,18 +2,19 @@
  * @Author: SessyoinChen
  * @Date: 2023-03-01 09:34:09
  * @LastEditors: SessyoinChen
- * @LastEditTime: 2023-03-26 13:31:16
+ * @LastEditTime: 2023-03-26 20:16:14
  * @FilePath: \6Semestre\TrabalhoDeConclusao\App.js
  * @Description: 
  * 
  */
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import Login from './src/components/login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import axios from 'axios'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
@@ -22,17 +23,20 @@ function App() {
     // </View>
     <NavigationContainer>
       <Stack.Navigator>
-
-        <Stack.Screen name="Los Hermanos" component={Login} />
         <Stack.Screen name="Cardápio" component={HomeScreen} />
+        <Stack.Screen name="Los Hermanos" component={Login} />
+
 
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-// Página inicial
+// Cardápio
 function HomeScreen({ navigation }) {
   const [data, setData] = React.useState([])
+  // const { width } = Dimensions.get('window');
+  // const MAX_WIDTH = width;
+
 
   React.useEffect(() => {
     axios.get('https://api.mercadolibre.com/sites/MLB/search?q=celular').then(res => {
@@ -40,13 +44,16 @@ function HomeScreen({ navigation }) {
       setData(res.data.results)
     })
   }, [])
+  
   return (
-    
-    <View>
+
+    <View style={styles.container}>
       {
         data.map(item => <div key={item.id}>
-          <h4>{item.title}</h4>
-          <img src={item.thumbnail} alt="" />
+          <TouchableOpacity style={styles.elemento}>
+            <img src={item.thumbnail} alt="" className='p-2' />
+            <Text style={styles.texto} numberOfLines={2} >{item.title}</Text>
+          </TouchableOpacity>
         </div>)
       }
     </View>
@@ -59,6 +66,17 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'row',
+    flex:1,
+    maxWidth: Dimensions.get('window')
   },
+  // elemento: {
+  //   width: '100%',
+  //   height: '100%'
+  // },
+  texto: {
+    padding: 2,
+    margin: 2,
+    fontSize: 20,
+  }
 });
