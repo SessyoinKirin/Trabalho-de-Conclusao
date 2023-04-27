6ccb01bb5ce508d2c3812a7089371bd7ea462fde
Haohui Chen <neosdiva@gmail.com>
2023-04-27 12:07:51 -0300

    foi adicionado a somatoria dos preços de itens em pagina de carrinho

--- src/components/carrinho/index.js
+++ src/components/carrinho/index.js
@@ -2,12 +2,12 @@
  * @Author: SessyoinChen
  * @Date: 2023-03-27 14:14:46
  * @LastEditors: SessyoinChen
- * @LastEditTime: 2023-04-25 17:18:38
+ * @LastEditTime: 2023-04-27 12:03:38
  * @FilePath: \Trabalho-de-Conclusao\src\components\carrinho\index.js
  * @Description: 
  * 
  */
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image, FlatList } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -20,16 +20,16 @@ export default function Carrinho({ route, navigation }) {
     const [currentMesaIndex, setCurrentMesaIndex] = useState(mesaIndex)
     // console.log(route.params, 'parametro')
     const [lista, setLista] = useState([]);
-
+    console.log(mesaIndex, 'index')
     useMemo(() => {
-      if (item) {
-        setLista(prevState => [...prevState, item]);
-        console.log(mesaIndex,'index da mesa', currentMesaIndex, 'curretn')
-      }
+        if (item) {
+            setLista(prevState => [...prevState, item]);
+            console.log(mesaIndex, 'index da mesa', currentMesaIndex, 'curretn')
+        }
     }, [item]);
 
 
-    useMemo(() => {
+    useEffect(() => {
         navigation.setOptions({
             headerTitle: `Mesa ${currentMesaIndex}`,
             headerStyle: {
@@ -39,26 +39,40 @@ export default function Carrinho({ route, navigation }) {
                 fontWeight: 'bold',
             },
         });
-        console.log(currentMesaIndex, 'currente', mesaIndex, 'mesaindex')
-    }, [currentMesaIndex]);
+        if (currentMesaIndex !== mesaIndex) {
+            navigation.setOptions({
+                headerTitle: `Mesa ${currentMesaIndex}`,
+                headerStyle: {
+                    backgroundColor: 'black',
+                },
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            });
+        }
+    }, [currentMesaIndex, mesaIndex, navigation]);
 
-function ListItem({item}){
-    return<View style={styles.cardapioItem}>
-    <View style={{ flex: 2 }}><Image src={item.thumbnail} alt="" style={[styles.cardapioImg, { width: 100, height: 100 }]} /></View>
+    const valorTotal = useMemo(() => {
+        return lista.reduce((acc, cur) => acc + cur.price, 0);
+      }, [lista]);
 
-    <View style={[styles.cardapioDescBtn, { flex: 5 }]}>
-      <View style={[styles.cardapioTitulo, { flex: 3 }]}>
-        <Text style={styles.cardapioTexto}>{item.title}</Text>
-      </View>
-      <View style={[styles.cardapioContador, { flex: 3 }]}>
-        
-        
-      </View>
-    </View>
+    function ListItem({ item }) {
+        return <View style={styles.cardapioItem}>
+            <View style={{ flex: 2 }}><Image src={item.thumbnail} alt="" style={[styles.cardapioImg, { width: 100, height: 100 }]} /></View>
+
+            <View style={[styles.cardapioDescBtn, { flex: 5 }]}>
+                <View style={[styles.cardapioTitulo, { flex: 3 }]}>
+                    <Text style={styles.cardapioTexto}>{item.title}</Text>
+                </View>
+                <View style={[styles.cardapioContador, { flex: 3 }]}>
+                <Text  style={styles.cardapioTexto}>{item.price}</Text>
+
+                </View>
+            </View>
 
 
-  </View>
-}
+        </View>
+    }
     // setLista(...[], item)
     return (
         <View style={styles.container}>
@@ -67,7 +81,7 @@ function ListItem({item}){
                     <Text style={styles.carLetra}>Adicionar</Text>
                 </TouchableOpacity>
                 <View style={{ flex: 5.5 }}>
-                    
+
                     <FlatList
                         data={lista}
                         renderItem={({ item }) => <ListItem item={item} />}
@@ -77,22 +91,27 @@ function ListItem({item}){
 
             </View>
             <View style={styles.carrinhoRotape}>
-
-                <Text style={styles.carTexto}>Valor total:</Text>
-                <View style={styles.carFlexend}>
-                    <View style={{ width: '50%', height: '100%' }}>
-                        <TouchableOpacity style={styles.carBotoesInfo}>
-                            <Text style={styles.carTextoBotao}>Enviar</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ width: '50%', height: '100%' }}>
-                        <TouchableOpacity style={styles.carBotoesSuccess}>
-                            <Text style={styles.carTextoBotao}>Finalizar</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
+                {item ? (
+                    <>
+                        <Text style={styles.carTexto}>Valor total: {valorTotal}</Text>
+                        <View style={styles.carFlexend}>
+                            <View style={{ width: '50%', height: '100%' }}>
+                                <TouchableOpacity style={styles.carBotoesInfo}>
+                                    <Text style={styles.carTextoBotao}>Enviar</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={{ width: '50%', height: '100%' }}>
+                                <TouchableOpacity style={styles.carBotoesSuccess}>
+                                    <Text style={styles.carTextoBotao}>Finalizar</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </>
+                ) : (
+                    <Text  style={[styles.cardapioTexto, {alignSelf:'center', padding:10}]}>Nenhum item encontrado.</Text>
+                )}
             </View>
+
         </View>
     )
 }
